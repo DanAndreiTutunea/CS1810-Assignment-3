02689ef32cf4e7fb414b7b342cf564c9a38dec32
Dan Andrei Tutunea <dan.andrei.tutunea@icloud.com>
2017-02-19 18:47:50 +0000

    some changes

---
+++ CS1810_opt2_drawing.java
@@ -0,0 +1,46 @@
+
+import edu.cmu.ri.createlab.terk.robot.finch.Finch;
+
+
+public class CS1810_opt2_drawing {
+
+
+	public static void main(final String[] args)
+    {
+    // Instantiating the Finch object
+    Finch myFinch = new Finch();
+
+    // Write some code here!
+
+    // Always end your program with finch.quit()
+    myFinch.quit();
+    System.exit(0);
+    }
+
+/*
+     public static String[] readCommand(){
+
+         //instructs the user to input data and reads the data
+
+     }
+
+     public static void printCommandList( args TBD ){
+         
+         //prints the list of executed commands before exiting
+
+     }
+
+     public static int executeMovement(Finch John, path move){
+         
+         //executes the movement as per the object received while making sure the movement is not interupted
+
+     }
+
+     public static boolean obstacle(Finch John){
+
+         //checks for obstacles
+
+     }
+*/
+
+}
